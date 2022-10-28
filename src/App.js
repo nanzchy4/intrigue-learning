08c652a06c6fb2af208ffad05d5bc66c42b6211c
Nanzi Mustafa Chowdhury <ge42poj@mytum.de>
2022-10-28 16:14:14 +0600

    login page and register page design done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layout/Main';
 import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path:'/login',
         element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
       }
     ]
     
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+import { FaGoogle, FaGithub } from 'react-icons/fa';
 
 const Login = () => {
     return (
         <div>
-            
+            <div>
             <Form className="w-50 mx-auto my-5">
-            <h1 className='mb-4'>Please Login</h1>
-                <Form.Group className="mb-3">
-                    <Form.Label>Full Name</Form.Label>
-                    <Form.Control type="email" placeholder="Enter full name" />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label>Photo URL</Form.Label>
-                    <Form.Control type="email" placeholder="Enter photo URL" />
-                </Form.Group>
+                <h1 className='mb-4'>Please Login</h1>
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" required />
@@ -23,15 +23,25 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required/>
+                    <Form.Control type="password" placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+                <p>Already have an account?<Link to='/register'>Register Here</Link></p>
                 <Button variant="warning" type="submit">
-                    Submit
+                    Login
                 </Button>
             </Form>
+
+            <div className='w-50 mx-auto'>
+            <button type="button" className="d-block mb-2 btn btn-success"><FaGoogle></FaGoogle> Sign In With Google</button>
+            <button type="button" className="d-block btn btn-success"><FaGithub></FaGithub> Sign In With GitHub</button>
+                
+            </div>
+
+
+            </div>
+
+            
+            
         </div>
     );
 };
